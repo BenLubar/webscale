1277596d5619a6b9b3abf9393db940ff179fc280
Ben Lubar <ben.lubar@gmail.com>
2016-06-19 19:39:23 -0500

    increase the breadcrumb font size to be more legible

--- static/style.css.gen.go
+++ static/style.css.gen.go
@@ -3,5 +3,5 @@
 package static
 
 var _ = css(asset{Name: "style.css", Content: "" +
-	"header>h1{margin:0}.breadcrumb{padding-left:0;text-transform:uppercase;font:8pt sans-serif}.breadcrumb>li{list-style:none;display:inline}.breadcrumb>li:not(:last-child):after{content:' \\203A '}.breadcrumb>li:last-child{font-weight:700}" +
-	"", etag: `"v0zgJ5kQDIs="`})
+	"header>h1{margin:0}.breadcrumb{padding-left:0;text-transform:uppercase;font:.8em sans-serif}.breadcrumb>li{list-style:none;display:inline}.breadcrumb>li:not(:last-child):after{content:' \\203A '}.breadcrumb>li:last-child{font-weight:700}" +
+	"", etag: `"ho+4Kahi2Rk="`})
